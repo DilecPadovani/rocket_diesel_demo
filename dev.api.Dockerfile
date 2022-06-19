11e911ff852b8d46bbbd7267abc4b1c3a3308dff
Dilec <dilec.padovani@gmail.com>
2022-06-19 14:13:23 +0200

    Implemented `docker-compose` use.

--- dev.api.Dockerfile
+++ dev.api.Dockerfile
@@ -1,11 +1,10 @@
 FROM rustlang/rust:nightly
 RUN apt install -y libpq-dev git
+WORKDIR /~
 COPY Rocket.toml .
 COPY Cargo.toml .
 COPY diesel.toml .
-COPY src .
-COPY migrations .
-RUN CARGO_NET_GIT_FETCH_WITH_CLI=true cargo install diesel_cli --no-default-features --features postgres
-RUN diesel migration run --database-url=postgres://postgres:password_secure@localhost/test_app_db
+COPY src src
+COPY migrations migrations
 RUN cargo build --release
-ENTRYPOINT ["target/release/rocket_diesel_demo"]
+ENTRYPOINT ["target/release/rocket_diesel_demo"]    
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
 mod database;
 mod schema;
 
 use database::models::NewCounter;
-use rocket::*;
+use rocket::{fairing::AdHoc, *};
 use rocket_contrib::databases::{database, diesel::PgConnection};
-
 // to show how request guard works in rocket: usefull to implicitly check requirements
 // const TOKEN: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/token"));
 #[database("test_db")]
@@ -30,7 +31,7 @@ fn add(name: String, number: u32, conn: DbConn) -> String {
     };
     let x = database::actions::add(&*conn, counter);
 
-    format!("fatto add {:?}", x)
+    format!("Added {:?}", x)
 }
 
 #[get("/subtract/<name>/<number>")]
@@ -41,18 +42,32 @@ fn subtract(name: String, number: u32, conn: DbConn) -> String {
     };
     let x = database::actions::subtract(&*conn, counter);
 
-    format!("fatto sub {:?}", x)
+    format!("Subtracted: {:?}", x)
 }
 
 #[get("/status/<name>")]
 fn status(name: String, conn: DbConn) -> String {
     let x = database::actions::get_counter_by_name(&*conn, name);
-    format!("Hello, {:?} year old named !", x)
+    format!("Hello, {:?} ", x)
 }
 
+fn run_db_migrations<'r>(rocket: &'r Rocket) {
+    let conn = DbConn::get_one(&rocket).expect("database connection");
+    diesel_migrations::embed_migrations!();
+
+    if let Err(e) = embedded_migrations::run(&*conn) {
+        eprintln!("Failed to run database migrations: {:?}", e);
+    }
+}
 fn main() {
+    // diesel_migrations::embed_migrations!();
+    // embedded_migrations::run();
     rocket::ignite()
         .attach(DbConn::fairing())
+        .attach(AdHoc::on_launch(
+            "Initialise server schema",
+            run_db_migrations,
+        ))
         .mount("/", routes![all, add, subtract, status])
         .launch();
 }
